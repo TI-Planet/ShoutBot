f48ac0cb7fd70c875806a4eca7c306ddd44ef6e4
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2021-02-11 17:25:55 +0100

    ✨ Introduce default bot

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import json
+
+from discord.ext import commands
+
+
+
+def action() :
+	print("test")
+
+__version__ = "under developpement"
+
+# Configuration
+with open("config.json", "r") as file:
+	config = json.load(file)
+
+
+bot = commands.Bot(command_prefix=config["PREFIX"])
+
+
+@bot.event
+async def on_ready():
+	print(f"Bot {bot.user.name} connected on {len(bot.guilds)} servers")
+	
+
+
+@bot.event
+async def on_message(message):
+	if message.author == bot.user:
+		return
+
+	if (message.channel.id == config["SHOUTBOX"]["channel"]):
+		await message.channel.send(message.content)
+
+
+bot.run(config["TOKEN"])
