ed68c7f9c84cde61751e26413b0d792f74c76aca
Myja <Myja@SNOW.local>
2021-02-14 13:57:45 +0100

    🚧 files and images from discord are sent to tiplanet
    images use thumbnails hardcoded at 50x50
    values should be in config and not hardcoded
    and proportions should be kept

--- src/bonfire.py
+++ src/bonfire.py
@@ -3,7 +3,7 @@ class bonfire:
 		self.config = config
 		self.chat = chat
 		self.bot = bot
-	
+
 
 	def updateChat(self, message):
 		if message.author == self.bot.user or str(message.webhook_id) == str(self.config["TIPLANET"]["webhook"]["id"]):
@@ -14,12 +14,41 @@ class bonfire:
 
 
 	def generateMessage(self, message):
+		quotePrefix = ''
+		attachmentSuffix = ''
+
+		# this is a reply, add quote
 		if (message.reference):
 			ref = message.reference
-			return f"[b]{self.removeDiscordID(message.author)}[/b] : [quote={self.removeDiscordID(ref.resolved.author)}]{ref.resolved.content}[/quote] {message.content}"
-		else : 
-			return f"[b]{self.removeDiscordID(message.author)}[/b] : {message.content}"
+			quotePrefix = f'[quote={self.removeDiscordID(ref.resolved.author)}]{ref.resolved.content}[/quote] '
 
+		# this contains files
+		if (message.attachments != None and len(message.attachments) != 0):
+			attachmentSuffix = '\n'.join([attachmentToString(a) for a in message.attachments])
+			attachmentSuffix = f'\n{attachmentSuffix}'
+
+		return f"[b]{self.removeDiscordID(message.author)}[/b] : {quotePrefix}{message.content}{attachmentSuffix}"
 
 	def removeDiscordID(self, username):
 		return str(username)[0:-5]
+
+def attachmentToString(attachment):
+	extension = attachment.url.split('.')[-1]
+
+	if attachment.width != None and extension in ['png', 'jpg', 'jpeg', 'gif', 'bmp']:
+		width, height = thumbnailDimensions(attachment.width, attachment.height)
+		ret = f'[url={attachment.url}][img]{attachment.proxy_url}?width={width}&height={height}[/img][/url]'
+	else:
+		basename = attachment.url.split('/')[-1]
+		ret = f'[url={attachment.url}]{basename}[/url]'
+
+	if attachment.is_spoiler():
+		ret = f'[ispoiler]{ret}[/ispoiler]'
+
+	return ret
+
+def thumbnailDimensions(width, height):
+	# TODO
+	# - use config rather than hardcode values
+	# - keep proportions
+	return (50, 50)
