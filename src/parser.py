51662e8df6892c45f3eae7c1273d906c57bea2e7
Hayleia <7904776+Hayleia@users.noreply.github.com>
2021-03-05 19:59:49 +0100

    ✨ parse /roll messages from TI-Bot

--- src/parser.py
+++ src/parser.py
@@ -64,6 +64,11 @@ class Parser:
 		self.bbcode2markdown.add_formatter('url', render_url, strip=True, swallow_trailing_newline=True)
 
 	def parse_bbcode2markdown(self, msg, id):
+		if id == self.config.TiBotId and msg.startswith('/roll '):
+			split = msg.split()
+			if len(split) == 4:
+				return f'{split[1]} lance {split[2]} et obtient {split[3]}.'
+
 		# shortcut completions and other quick changes
 		msg = msg.replace('[url=/', '[url=https://tiplanet.org/')
 		msg = msg.replace('[img]/', '[img]https://tiplanet.org/')
